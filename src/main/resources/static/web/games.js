b2212ba4e437e05c995aac5bf86ac910b934a921
Christin Bergmann <christin.bergmann@gmx.net>
2019-12-13 15:20:52 +0100

    fixed ul for games

--- src/main/resources/static/web/games.js
+++ src/main/resources/static/web/games.js
@@ -1,18 +1,38 @@
-   async function getData() {
-       //await the response of the fetch call
-       let response = await fetch('http://localhost:8080/api/games');
-       console.log(response);
-       //proceed once the first promise is resolved.
-       let data = await response.json()
-       //proceed only when the second promise is resolved
-       return data;
-   }
-   //call getData function
-   getData()
-       .then(data => console.log(data)); //log the data
+async function getData() {
+    //await the response of the fetch call
+    let response = await fetch('http://localhost:8080/api/games');
+    console.log(response);
+    //proceed once the first promise is resolved.
+    let data = await response.json()
+    //proceed only when the second promise is resolved
+    return data;
+}
+//call getData function
+getData()
+    .then(data => {
+        console.log(data);
 
-   //    async function helper() {
-   //        let games = await getData();
-   //        console.log("games", games)
-   //    }
-   //    helper()
+        let gamesList = document.getElementById("games")
+
+        data.forEach(game => {
+            let ul = document.createElement("ul")
+            ul.innerHTML = "Game No. : " + game.Game_Id
+            gamesList.appendChild(ul);
+
+            game.GamePlayers.forEach(gamePlayer => {
+                console.log(gamePlayer)
+
+                let listPlayer = document.createElement("li")
+                listPlayer.innerHTML = gamePlayer.GamePlayer_Id
+                ul.appendChild(listPlayer);
+
+
+                let listEmail = document.createElement("li")
+                listEmail.innerHTML = "Player : " + gamePlayer.Player[0].Player_Username
+                ul.appendChild(listEmail);
+                console.log(gamePlayer.Player[0].Player_Username);
+
+            })
+        })
+
+    });
